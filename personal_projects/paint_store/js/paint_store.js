9ffc7dcf8ee990fb6fa2b8b1ea0f0ba0c3fe5540
Eric Nolte <amishe@gmail.com>
2014-08-18 14:35:10 -0500

    update paint store game

--- personal_projects/paint_store/js/paint_store.js
+++ personal_projects/paint_store/js/paint_store.js
@@ -9,6 +9,13 @@
 	// 	};
 	// });
 
+	app.directive("newGame", function() {
+		return {
+			restrict:"E",
+			templateUrl:"new_game.html",
+		};
+	});
+
 	app.directive("game", function() {
 		return {
 			restrict:"E",
@@ -16,32 +23,108 @@
 		};
 	});
 
-	app.controller("gameController", function() {
+	app.directive("gameOver", function() {
+		return {
+			restrict:"E",
+			templateUrl:"game_over.html",
+		};
+	});
+
+	app.controller("GameCtrl", function($interval, $scope) {
 		var self = this;
+		this.gameState = "newGame";
+		var swatchR;
+		var swatchG;
+		var swatchB;
+		var paintR = 0;
+		var paintG = 0;
+		var paintB = 0;
+		this.score = 0;
+		this.customers = 0;
+		this.timeLimit = 120;
+		this.timeleft;
 
 
-		this.createCustomerSwatch = function() {
-			r = 100;//Math.floor(Math.random() * 255);
-			g = 100;//Math.floor(Math.random() * 255);
-			b = 100;//Math.floor(Math.random() * 255);
-			$("#customer-swatch").css("background-color", "rgb(" + r + "," + g + "," + b + ")");
+		this.giveToCustomer = function() {
+			self.preview();
+			self.score += self.scoreColor();
+			self.customers++;
+			self.newSwatch();
+
+
 		};
 
-		this.createGuessSwatch = function() {
-			r = $("#rGuess").val();
-			g = $("#gGuess").val();
-			b = $("#bGuess").val();
-			$("#user-swatch").css("background-color", "rgb(" + r + "," + g + "," + b + ")" );
-		};
-
-		this.guess = function() {
-			self.createGuessSwatch();
-			var userColor = $("#user-swatch").css("background-color");
-			var customerColor = $("#customer-swatch").css("background-color");
-			if (userColor == customerColor) {
-				console.log("Correct Answer!");
-			}
+		this.newSwatch = function() {
+			swatchR = Math.floor(Math.random() * 255);
+			swatchG = Math.floor(Math.random() * 255);
+			swatchB = Math.floor(Math.random() * 255);
+			$("#customer-swatch").css("background-color", "rgb(" + swatchR + "," + swatchG + "," +
+				swatchB + ")");
 		}
+
+		this.preview = function() {
+			paintR = $("#rGuess").val();
+			paintG = $("#gGuess").val();
+			paintB = $("#bGuess").val();
+			$("#paint-can").css("background-color", "rgb(" + paintR + "," + paintG +
+				"," + paintB + ")" );
+		}
+
+		this.scoreColor = function() {
+			score = 200;
+			rVarience = Math.abs(swatchR - paintR);
+			gVarience = Math.abs(swatchG - paintG);
+			bVarience = Math.abs(swatchB - paintB);
+			console.log(rVarience);
+			score = score - rVarience - gVarience - bVarience;
+			return score;
+		}
+
+
+
+
+		this.newGame = function() {
+			return this.gameState === "newGame";
+		}
+
+		this.startNewGame = function() {
+			this.gameState = "gameInProgress";
+			init();
+		}
+
+		this.gameInProgress = function() {
+			return this.gameState === "gameInProgress";
+		}
+
+		this.gameOver = function() {
+			return this.gameState === "gameOver";
+		}
+
+		this.gameOverScreen = function() {
+			this.gameState = "gameOver";
+			$interval.cancel(countdown);
+		}
+
+		this.countdown = $interval(function(){
+				self.timeleft--;
+				if (self.timeleft === 0) {
+					self.gameOverScreen();
+				}
+			}, 1000, 120);
+
+		var init = function($scope) {
+			self.newSwatch();
+			self.timeleft = self.timeLimit;
+			self.countdown();
+
+		}
+
+		this.initializeGame = function() {
+			init();
+		}
+
+
+
 
 	})
 

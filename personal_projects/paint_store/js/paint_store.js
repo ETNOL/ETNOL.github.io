6dcfd6654fe29e83533542c78c0e84c4bf9b05ec
Eric Nolte <eric.nolte@mediavine.com>
2020-08-20 09:40:13 -0400

    use range sliders for paint game

--- personal_projects/paint_store/js/paint_store.js
+++ personal_projects/paint_store/js/paint_store.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
 
 	// Initialize Angular Module //
 	var app = angular.module("paintStore", []);
@@ -6,30 +6,30 @@
 
 	// Basic Directives //
 
-	app.directive("newGame", function() {
+	app.directive("newGame", function () {
 		return {
-			restrict:"E",
-			templateUrl:"new_game.html",
+			restrict: "E",
+			templateUrl: "new_game.html",
 		};
 	});
 
-	app.directive("game", function() {
+	app.directive("game", function () {
 		return {
-			restrict:"E",
-			templateUrl:"game.html",
+			restrict: "E",
+			templateUrl: "game.html",
 		};
 	});
 
-	app.directive("gameOver", function() {
+	app.directive("gameOver", function () {
 		return {
-			restrict:"E",
-			templateUrl:"game_over.html",
+			restrict: "E",
+			templateUrl: "game_over.html",
 		};
 	});
 
 	// Game Controller //
 
-	app.controller("GameCtrl", function($interval, $scope) {
+	app.controller("GameCtrl", function ($interval, $scope) {
 		// Using "self" to control for possible scope problems //
 		var self = this;
 		// Only 1 directive can be displayed at a time. gameState controls which is displayed //
@@ -55,7 +55,7 @@
 
 
 		// Main Game Functions //
-		this.giveToCustomer = function() {
+		this.giveToCustomer = function () {
 			self.preview();
 			self.updateScore();
 			self.customers++;
@@ -68,7 +68,7 @@
 			self.score += self.lastScore;
 		}
 
-		this.newSwatch = function() {
+		this.newSwatch = function () {
 			swatchR = Math.floor(Math.random() * 255);
 			swatchG = Math.floor(Math.random() * 255);
 			swatchB = Math.floor(Math.random() * 255);
@@ -76,19 +76,19 @@
 				swatchB + ")");
 		}
 
-		this.preview = function() {
+		this.preview = function () {
 			paintR = $("#rGuess").val();
 			paintG = $("#gGuess").val();
 			paintB = $("#bGuess").val();
 			$("#paint-can").css("background-color", "rgb(" + paintR + "," + paintG +
-				"," + paintB + ")" );
+				"," + paintB + ")");
 		}
 		// Sets a base score of 200, deducts for each point of variance between the swatch
 		// values and the customer's paint guess values //
 		// BUG: Possible to spam a guess of ~130 and slowly increase score over enough guesses.
 		// I ran up a score of around 3k+ over 832 customers this way.
-		this.scoreColor = function() {
-			score = 200;
+		this.scoreColor = function () {
+			score = 100;
 			rVarience = Math.abs(swatchR - paintR);
 			gVarience = Math.abs(swatchG - paintG);
 			bVarience = Math.abs(swatchB - paintB);
@@ -98,7 +98,8 @@
 		}
 		// Needs to be refactored... Too many responsibilities.
 		// Performs countdown, triggers 'game over' if 0, triggers red time text if under 10.
-		this.countdown = function() {	$interval(function(){
+		this.countdown = function () {
+			$interval(function () {
 				self.timeleft--;
 				if (self.timeleft === 0) {
 					self.gameOverScreen();
@@ -107,7 +108,7 @@
 					self.textColor = "red";
 				}
 				else {
-					self.textColor ="black";
+					self.textColor = "black";
 				}
 			}, 1000, self.timeLimit);
 		}
@@ -116,31 +117,31 @@
 		// Basic setter / getter directive controls //
 		// No setter for new game, directive only shows once at initial page load //
 
-		this.newGame = function() {
+		this.newGame = function () {
 			return this.gameState === "newGame";
 		}
 
-		this.startNewGame = function() {
+		this.startNewGame = function () {
 			this.gameState = "gameInProgress";
 			init();
 		}
 
-		this.gameInProgress = function() {
+		this.gameInProgress = function () {
 			return this.gameState === "gameInProgress";
 		}
 
-		this.gameOver = function() {
+		this.gameOver = function () {
 			return this.gameState === "gameOver";
 		}
 
-		this.gameOverScreen = function() {
+		this.gameOverScreen = function () {
 			this.gameState = "gameOver";
 			$interval.cancel(self.countdown); // Does not work, timer ends via last $interval param
 		}
 
 		// Initialization Functions //
 
-		var init = function() {
+		var init = function () {
 			self.score = 0;
 			self.lastScore = 0;
 			self.newSwatch();
@@ -150,15 +151,15 @@
 
 		}
 
-		this.initializeGame = function() {
+		this.initializeGame = function () {
 			init();
 		}
 
 		// Game Effects //
 
-		this.flashScore = function() {
-			$('#scoreFlash').css("visibility", "visible").fadeOut(400, function() {
-				$('#scoreFlash').css("display","inline").css("visibility", "hidden");
+		this.flashScore = function () {
+			$('#scoreFlash').css("visibility", "visible").fadeOut(400, function () {
+				$('#scoreFlash').css("display", "inline").css("visibility", "hidden");
 			});
 		}
 
